41d4dea014dc762825a903cf996bbf680f5d0e80
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-10-25 15:42:13 -0600

    Se creó la vista de categorías.

---
+++ app/src/main/java/sv/edu/catolica/mh_prueba2_proyecto/Categorias.java
@@ -0,0 +1,17 @@
+package sv.edu.catolica.mh_prueba2_proyecto;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+
+public class Categorias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categorias);
+
+    }
+}
